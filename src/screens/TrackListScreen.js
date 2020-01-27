fd4600998e3ae4ad13fbd3e084c5dcde4df96339
Jason Vance <jbvance@gmail.com>
2020-01-27 17:54:51 -0600

    add SafeAreaView component to TrackDetailScreen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 
 const TrackListScreen = ({ navigation }) => {
-    return <>
+    return <React.Fragment>
        <Text style={{fontSize: 48 }}>TrackListScreen</Text>
        <Button title="Go to track detail" onPress={() => navigation.navigate('TrackDetail')} />
-    </>;
+       </React.Fragment>;
 };
 
 const styles = StyleSheet.create({});
