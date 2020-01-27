bf70a852b2e4d633c541419b0246069873cbc67e
Jason Vance <jbvance@gmail.com>
2020-01-27 06:33:03 -0600

    add signup form

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
-    return (
+const TrackListScreen = ({ navigation }) => {
+    return <>
        <Text style={{fontSize: 48 }}>TrackListScreen</Text>
-    );
+       <Button title="Go to track detail" onPress={() => navigation.navigate('TrackDetail')} />
+    </>;
 };
 
 const styles = StyleSheet.create({});
