1a306fef21049cae64659fa3d595ce9711574d42
Jason Vance <jbvance@gmail.com>
2020-01-27 08:32:19 -0600

    begin signup workflow

---
+++ src/context/AuthContext.js
@@ -0,0 +1,44 @@
+import { AsyncStorage } from 'react-native';
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload }
+    default:
+      return state;
+  }
+};
+
+const signup = dispatch => async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+      await AsyncStorage.setItem('token', response.data.token);
+      dispatch({type: 'signup', payload: response.data.token});
+    } catch (err) {
+      dispatch({
+        type: 'add_error',
+        payload: 'Sorry, something went wrong with sign up'
+      });
+      console.log(err.message);
+    }
+  };
+
+const signin = dispatch => {
+  return ({ email, password }) => {};
+};
+
+const signout = dispatch => {
+  return () => {
+    //sign out
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signup, signin, signout },
+  { token: null, errorMessage: '' }
+);
